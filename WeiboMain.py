4e07aeb61feeda74e923b614a9fa1690d9dce6d2
quantin <quantin@QuandragondeMacBook-Pro.local>
2015-01-20 17:08:46 +0800

    通过Url访问用户

--- WeiboMain.py
+++ WeiboMain.py
@@ -5,6 +5,8 @@ __author__ = 'Quantin Hsu'
 
 import requests
 from bs4 import BeautifulSoup
+from Queue import Queue
+import threading
 import re
 import time
 import os
@@ -17,14 +19,44 @@ dirName = '/Users/quantin/data/weibo/'
 startUrl = 'http://weibo.cn/yaochen'
 # 用户信息文件
 userFile = open(dirName + 'userinfo.txt', 'a+')
+
+# 已经爬取的用户Id列表
+alreadyCrawl = []
+
+# 还未爬取的用户Id队列
+readyToCrawl = Quene()
+
 # postHeader = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; rv:24.0) Gecko/20100101 Firefox/24.0'}
+
+
+# 没爬过的用户写入文件
+def writeToFile(weiboUser):
+	if weiboUser.userId in alreadyCrawl:
+		print 'haved!!!!!'
+		return
+	else:
+		#print weiboUser.userId + weiboUser.otherInfo
+		userFile.write(weiboUser.userId + ' ' + weiboUser.otherInfo + ' ' + time.strftime('%Y-%m-%d %H:%M:%S') + '\n')
+		alreayCrawl.append(weiboUser.userId)
+
+def breadthFirstTraversal(weiboUser):
+	readyToCrawl.append(weiboUser.userId)
+	weiboUser.getFollowsList(cookies)
+
+
+# 拿到cookies和起始url之后开始爬取
+def startCrawl(originUrl, cookies):
+	weiboUser = WeiboUser.WeiboUser(-1)
+	weiboUser.getUserInfo('http://weibo.cn/yaochen', cookies)
+
+	breadthFirstTraversal(weiboUser)
 
 if __name__ == '__main__':
 	weiboLogin = WeiboLogin('18601346913', 'testsina')
 	cookies = weiboLogin.getCookie()
-	weiboUser = WeiboUser(-1)
-	weiboUser.getUserInfo('http://weibo.cn/yaochen', cookies)
-	print weiboUser.id
+	startCrawl('http://weibo.cn/yaochen', cookies)
+	
+
 
 
 
--- WeiboUser.py
+++ WeiboUser.py
@@ -14,8 +14,8 @@ postHeader = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) App
 
 class WeiboUser:
     'USER INFO'
-    def __init__(self, id):
-        self.id = id # 用户唯一id
+    def __init__(self, userId):
+        self.userId = userId # 用户唯一id
         # self.otherInfo = otherInfo
         # self.username = username # 姓名
         # self.urank = urank # 等级
@@ -35,22 +35,58 @@ class WeiboUser:
         userinfo = spaninfo.encode('utf-8').strip().split(r' ')
         user = userinfo[0].replace('/', " ")
         idinfo = (mainSoup.find_all('img', 'por')[0])['src']
-        self.id = idinfo[22:32].encode('utf-8')
+        self.userId = idinfo[22:32].encode('utf-8')
         # 需要判断id是否已经读过
         mcountinfo = (mainSoup.find_all('span', 'tc')[0]).text
         mcount = mcountinfo[3:-1].encode('utf-8')
-        followkey = '/' + self.id + '/' + 'follow'
+        followkey = '/' + self.userId + '/' + 'follow'
         followsnuminfo = (mainSoup.find_all(attrs={"href": followkey})[0]).text
         self.followsnum = followsnuminfo[3:-1].encode('utf-8')
-        fanskey = '/' + self.id + '/' + 'fans'
+        fanskey = '/' + self.userId + '/' + 'fans'
         fansnuminfo = (mainSoup.find_all(attrs={"href": fanskey})[0]).text
         fansnum = fansnuminfo[3:-1].encode('utf-8')
         self.otherInfo = user + ' ' + mcount + ' ' + self.followsnum + ' ' + fansnum
-        print self.id + ' ' + self.otherInfo + '\n'
-        return self.id, self.otherInfo
+        print self.userId + ' ' + self.otherInfo + '\n'
+        return self.userId, self.otherInfo
 
+def getFollowsList(userId, cookies):
+        pageNumUrl = 'http://weibo.cn/' + userId + '/follow?page='
+        print pageNumUrl
+        # cookies = {
+        #     '_T_WM': '26a84199a1c84735dc3079aef4a95a1b',
+        #     'SUB': '_2A255ucYrDeTxGeNK7VUW-CnNwj-IHXVbRepjrDV6PUJbrdAKLWTbkW2C1UECqjaZCaYkkeZFTLBK9pQxJg..',
+        #     'gsid_CTandWM': '4uG9b2141YTtjaGftAPMDmWq737',
+        #     'M_WEIBOCN_PARAMS': 'rl%3D1'
+        # }
+        pageNumRespond = requests.get(pageNumUrl, cookies=cookies)
+        pageNumRespond.encoding = 'utf-8'
+        # print pageNumRespond.text
+
+        followsList = []
+        pageNumSoup = BeautifulSoup(pageNumRespond.text, 'lxml')
+        postForm = (pageNumSoup.find_all(id='pagelist')[0]).text
+        followPageNum = int(postForm[-3:-1])
+        print followPageNum
+
+        for i in range(1, followPageNum + 1):
+            followsUrl = pageNumUrl + str(i)
+            followsRespond = requests.get(followsUrl, cookies=cookies)
+            followsRespond.encoding = 'utf-8'
+
+            followSoup = BeautifulSoup(followsRespond.text, 'lxml')
+            # 通过关注他拿到关注人的id
+            # TODO：已经关注过了的人的拿不到id
+            followTag = followSoup.find_all(href=re.compile("http://weibo.cn/attention/add"))
+            for j,fTag in enumerate(followTag):
+                fId = fTag['href'][35:44]
+                print fId
+                followsList.append(fId)
+        return followsList
+
+
+'''
     def getFollowsList(self, cookies):
-        self.pageNumUrl = 'http://weibo.cn/' + self.id + '/follow?page='
+        self.pageNumUrl = 'http://weibo.cn/' + self.userId + '/follow?page='
         print self.pageNumUrl
         # cookies = {
         #     '_T_WM': '26a84199a1c84735dc3079aef4a95a1b',
@@ -62,7 +98,7 @@ class WeiboUser:
         pageNumRespond.encoding = 'utf-8'
         # print pageNumRespond.text
 
-        self.followsIdList = []
+        self.followsList = []
         pageNumSoup = BeautifulSoup(pageNumRespond.text, 'lxml')
         postForm = (pageNumSoup.find_all(id='pagelist')[0]).text
         followPageNum = int(postForm[-3:-1])
@@ -79,13 +115,13 @@ class WeiboUser:
             followTag = followSoup.find_all(href=re.compile("http://weibo.cn/attention/add"))
             for j,fTag in enumerate(followTag):
                 fId = fTag['href'][35:44]
-                self.followsIdList.append(fId)
-
+                self.followsList.append(fId)
+'''
 
 if __name__ == '__main__':
     weiboLogin = WeiboLogin('18601346913', 'testsina')
     cookies = weiboLogin.getCookie()
     weiboUser = WeiboUser(-1)
     userId, userOtherInfo = weiboUser.getUserInfo('http://weibo.cn/yaochen', cookies)
-    weiboUser.getFollowsList(cookies)
+    getFollowsList(userId, cookies)
         
